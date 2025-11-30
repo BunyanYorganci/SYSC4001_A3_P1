0cbcb53f36b2feace2cc298b3d7ff37a93cac11f
BunyanYorganci <bunyanyorganci@cmail.carleton.ca>
2025-11-30 05:47:18 -0500

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,9 @@
+if [ ! -d "bin" ]; then
+    mkdir bin
+else
+	rm bin/*
+fi
+
+g++ -g -O0 -I . -o bin/interrupts_EP interrupts_101216821_EP.cpp
+g++ -g -O0 -I . -o bin/interrupts_RR interrupts_101216821_RR.cpp
+g++ -g -O0 -I . -o bin/interrupts_EP_RR interrupts_101216821_EP_RR.cpp
